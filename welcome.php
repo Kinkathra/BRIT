3d6765ed8b5d30b8ac2aea5afc74685b7c959110
c4mingo <calvin.mingoh@gmail.com>
2022-03-10 22:17:07 -0600

    edit

--- welcome.php
+++ welcome.php
@@ -35,7 +35,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <p>
-        <a href="inventory.php" class="btn btn-primary">Backroom</a>
+        <a href="inventory.php" class="btn btn-primary">ENTER</a>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
